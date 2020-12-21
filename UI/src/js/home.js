6ccbed567260d0eab41a86c8edbe6b0325e13183
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2020-12-21 13:26:00 -0600

    Refresh tokens work

--- UI/src/js/home.js
+++ UI/src/js/home.js
@@ -5,54 +5,96 @@ var {
 var fs = require('fs');
 
 var serverisOn = false;
+var authSaved = false;
+var refreshed = false;
 var AuthHandle = require(app.getAppPath() + "/chatbot/lib/auth.js");
-AuthHandle.updatePath(app.getPath("userData"))
+AuthHandle.updatePath(app.getPath("userData"));
 
-function loadCharts() {
-    getBasicData()
+
+function rememberID() { // checks if an id has been entered for auth
+    console.log("Getting thier stored auth information.");
+       var auth = AuthHandle.readAuth();
+       auth.then( data => {
+           //Checks if any data is there, this is seeing if a file even exists.
+           if (data.length == 0) {
+               console.log("The auth file does not yet exist, creating it.")
+               AuthHandle.makeAuth() //Create the file with blank data. 
+               //Now we set the buttons/ inputs on the home page to be empty.
+               document.getElementById("clientID").removeAttribute("disabled");
+               document.getElementById("secretID").removeAttribute("disabled");
+               document.getElementById("saveAuth").setAttribute("onclick", "saveAuth()")
+               document.getElementById("saveAuth").innerHTML = "Save";
+               return;
+           }
+           //First we check if the client ID and secret exist. If they do we continue, else we ask for the IDs.
+           if (data[0].clientID.length < 2 || data[0].secret.length < 2) {
+               console.log("The client ID or secret do not yet exist in proper form. Please save them to the auth file!");
+               //Now we set the buttons/ inputs on the home page to be empty.
+               document.getElementById("clientID").removeAttribute("disabled");
+               document.getElementById("secretID").removeAttribute("disabled");
+               document.getElementById("saveAuth").setAttribute("onclick", "saveAuth()")
+               document.getElementById("saveAuth").innerHTML = "Save";
+           } else {
+            //Now we can safely assume that they have the client ID and secret saved. We reflect this on the buttons. 
+              document.getElementById("clientID").setAttribute("disabled", "");
+              document.getElementById("secretID").setAttribute("disabled", "");
+              document.getElementById("clientID").setAttribute("placeholder", "ID Saved!")
+              document.getElementById("secretID").setAttribute("placeholder", "ID Saved!")
+              document.getElementById("saveAuth").setAttribute("onclick", "editAuth()")
+              document.getElementById("saveAuth").innerHTML = "Edit Auth";
+              if (data[0].access_token.length > 5 && refreshed == false) { // They have a token as well as the other info, we need to refresh it.
+                    console.log("They alread have an access token, we will begin refreshing it.")
+                    AuthHandle.refreshToken(data[0].access_token, data[0].refresh_token, data[0].clientID, data[0].secret, data[0].code).then(refresh => {
+                        if (refresh == "SUCCESS") {
+                                refreshed = true;
+                                document.getElementById("authButton").innerHTML = "Auth has beeen refreshed. ";
+                        }
+                    })
+              }
+
+             if (data[0].access_token.length > 5 && refreshed == true) {
+                document.getElementById("authButton").innerHTML = "Auth has beeen refreshed. Ready to join chat.";
+             }
+           }
+       })
+    
 }
 
-function rememberID() {
-    console.log("Getting thier stored ID.");
-    try {
-       var possibleID = JSON.parse(fs.readFile(`${app.getPath("userData")}/data/auth.JSON`, function(err, data) {
-        possibleID = JSON.parse(data)
-        console.log(possibleID)
-        if (possibleID.clientID !== "" && possibleID.clientID !== undefined) {
-            AuthHandle.recieveID(possibleID.clientID)
-            document.getElementById("clientID").setAttribute("disabled", "");
-            document.getElementById("clientID").setAttribute("placeholder", "ID Saved!")
-            document.getElementById("saveAuth").setAttribute("onclick", "editAuth()")
-            document.getElementById("saveAuth").innerHTML = "Edit Auth";
-        } else {throw err}
-       } ))
-    } catch(e) {
-        console.log(e)
-        document.getElementById("clientID").removeAttribute("disabled");
-        document.getElementById("saveAuth").setAttribute("onclick", "saveAuth()")
-        document.getElementById("saveAuth").innerHTML = "Save";
-    }
-}
 
-function saveAuth() {
+function saveAuth() { //sets the state to saved
     var replacement = JSON.stringify({token: "", clientID: document.getElementById("clientID").value, refresh: null})
-    fs.writeFileSync(`${app.getPath("userData")}/data/auth.JSON`, replacement);
+ //   fs.writeFileSync(`${app.getPath("userData")}/data/auth.JSON`, replacement);
     document.getElementById("clientID").setAttribute("disabled", "");
-    document.getElementById("clientID").setAttribute("placeholder", "ID Saved!")
-    document.getElementById("clientID").value = "ID Saved!"
+    document.getElementById("secretID").setAttribute("disabled", "");
     document.getElementById("saveAuth").setAttribute("onclick", "editAuth()")
     document.getElementById("saveAuth").innerHTML = "Edit Auth";
-    AuthHandle.recieveID(document.getElementById("clientID").value)
+    AuthHandle.updateID(document.getElementById("clientID").value, document.getElementById("secretID").value)
+    // We set the value to "ID SAVED" because we don't want the ID showing, streamers would probably show it accidentally. 
+    document.getElementById("clientID").value = "ID Saved";
+    document.getElementById("secretID").value = "ID Saved";
 }
 
-function editAuth() {
+function editAuth() { //Sets the state to editable
     document.getElementById("clientID").removeAttribute("disabled");
+    document.getElementById("secretID").removeAttribute("disabled");
     document.getElementById("saveAuth").setAttribute("onclick", "saveAuth()")
     document.getElementById("saveAuth").innerHTML = "Save";
+    document.getElementById("clientID").value = "";
+    document.getElementById("secretID").value = "";
+    document.getElementById("clientID").setAttribute("placeholder", "Enter Client ID")
+    document.getElementById("secretID").setAttribute("placeholder", "Enter Secret ID")
+
 }
 
 function auth() {
-    let authSchema = JSON.parse(fs.readFileSync(`${app.getPath("userData")}/data/auth.JSON`))
-    AuthHandle.updatePath(app.getPath("userData"));
-    AuthHandle.Auth(authSchema) //runs the webserver so we can get the token needed to connect to chat.
+    AuthHandle.Auth() //runs the webserver so we can get the token needed to connect to chat. It will refresh if a token is avaible. 
+
+}
+
+
+
+
+// Charts
+function loadCharts() {
+    getBasicData()
 }
--- chatbot/lib/auth.js
+++ chatbot/lib/auth.js
@@ -1,15 +1,21 @@
 //This file handles connecting the users dev app to glimesh.tv
 //It creates an express server so glimesh can redirect the user back to the bot. It is closed when the auth is completed.
 var fs = require("fs")
-const open = require('open'); //Opens the browser.
+const open = require('open'); //Opens the users default browser. hopefully...
+var request = require("request")
 //express
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Datastore = require('nedb'); //Connects to the auth DB
 
 var path = "./";
-var serverisOn = false;
-var tempID = ""
+var serverisOn = false; // tells us if the user started but didn't complete the auth. 
+var client = "" //Client ID
+var secret = "" //secret ID
+var authDB; //Auth database containing all auth info 
+
+var token = {access_token: "", refresh_token: "", code: "", scope: "", creation: "", expire: ""}
 
 function startAuthServer(authScheme) {
   if (serverisOn == true) {console.log('server is already running, if this is a mistake restart glimboi');
@@ -24,15 +30,42 @@ app.get('/auth', (req, res) => {
   res.redirect(301, `https://glimesh.tv/oauth/authorize?response_type=code&state=&client_id=${authScheme.clientID}&scope=public%20email%20chat%20streamkey&redirect_uri=http://localhost:3000/success`);
 });
 
+//The user completed the auth
 app.get('/success', (req, res) => {
-  let code = req.query.code;
-  console.log("Auth complete! Code: " + code + 'Causing error to close the server');
-  res.send('<p>Auth complete! You can close this now. </p>');
-  code = JSON.stringify({token: code, clientID: tempID})
-  fs.writeFileSync(`${path}/data/auth.JSON`, code)
-  //Need to send an auth screen and wait a few seconds before xlosing.
-  app.listen(port, () => console.log('Closing auth server on port ' + port))
+  let code = req.query.code; //We use this to request a token
+  console.log("Auth complete! Code: " + code + ' Requesting token. Causing error to close the server');
+  res.send('<p>Auth complete! You can close this now. </p>'); //tells the users we completed auth. We still need to request a token though
+  var options = {
+    method: 'POST',
+    body: "",
+    url: "https://glimesh.tv/api/oauth/token?grant_type=authorization_code&code=" + code + "&redirect_uri=http://localhost:3000/success&client_id=" + client + "&client_secret=" + secret
+};
+//Requests a token with the code and other info. Written to the db after. The token var houses it for this session  
+request(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) { //If all is as it should be
+        console.log(body); //The unfiltered response
+        var data = JSON.parse(body);
+        console.log(data)
+       //Sets the token info to match the var. We use the var to authenicate.
+       token.access_token = data.access_token;
+       token.refresh_token = data.refresh_token
+       token.scope = data.scope
+       token.creation = data.created_at
+       token.expire = data.expires_in
+       //Updates the DB with the info
+       authDB.update({}, { $set: { code: code, access_token: data.access_token, refresh_token: data.refresh_token, created_at: data.created_at, expire: data.expires_in } }, { multi: true }, function (err, numReplaced) {
+        console.log("Got the tokens, ready to connect to glimesh")
+      });
+     //Kills the server with an error.
+       app.listen(port, () => console.log('Closing auth server on port ' + port))
+    } else {
+        console.log(error) //log any errors
+        console.log(response.statusCode)
+        console.log(body)
+    }
 });
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('App listening on port ' + port), serverisOn = true, open(`https://glimesh.tv/oauth/authorize?response_type=code&state=&client_id=${authScheme.clientID}&scope=public%20email%20chat%20streamkey&redirect_uri=http://localhost:3000/success`))
@@ -41,36 +74,92 @@ app.listen(port, () => console.log('App listening on port ' + port), serverisOn 
 
 //Gets an access token for the user. This lets the Bot control the account so it can respond to commands and do all functions.
 //It also lets the bot to API requests for any user.
-function Auth(authScheme) {
-    if (authScheme) {} else {
-      var authScheme = JSON.parse(fs.readFileSync(path + "/data/auth.JSON"));
-    }
-    startAuthServer(authScheme)
+function Auth() {
+    var authscheme = readAuth().then(data => {
+      client = data[0].clientID;
+      secret = data[0].secret
+      startAuthServer({clientID: data[0].clientID})
+    })
 }
 
 //Updates the file path. Electron and the non electron build use different URLS.
 function updatePath(GUI) {
   console.log("path is " + GUI);
   path = GUI;
+  authDB = new Datastore({ filename: `${path}/data/auth.db`, autoload: true });
 }
 
-function recieveID(id) {
-  tempID = id;
+//Updates the auth module with our IDs so we don't have to read the file again. 
+function recieveID(id, id2) {
+  client = id;
+  secret = id2
 }
 
-function checkForAuthFile() {
-  try {
-   var auth = JSON.parse(fs.readFileSync(path + "/data/auth.JSON"));
-   console.log(auth.clientID, auth.token);
-   console.log("File exists ^^")
-  } catch(e) {
-    console.log(e)
-    console.log("File does not exist ^^");
+//Reads the auth file 
+async function readAuth() {
+   return new Promise(resolve => {
+    authDB.find( {}, function (err, docs) {
+      console.log(docs)
+      resolve(docs)
+    });
+   })
+}
+
+//makes an auth file with content so it actually saves, still blank though
+async function makeAuth() {
+  return new Promise(resolve => {
+    authDB.insert({clientID: "", secret: "", code: "", access_token: "", refresh_token: "", created_at: "", expire: ""}, function (err, newDocs) {
+      console.log(newDocs)
+      resolve("finished!")
+    });
+  })
+  
+}
+
+// We refresh the access token and get a new one. Refresh tokens last for a year. 
+async function refreshToken(access_token, refresh_token, client_id, client_secret, code) {
+  return new Promise(resolve => {
+    var options = {
+      method: 'POST',
+      body: "",
+      url: "https://glimesh.tv/api/oauth/token?grant_type=refresh_token&refresh_token=" + refresh_token + "&redirect_uri=http://localhost:3000/success&client_id=" + client_id + "&client_secret=" + client_secret + "&code=" + code
+  };
+  //Refreshes the token
+  request(options, (error, response, body) => {
+      if (!error && response.statusCode == 200) { //If all is as it should be
+          var data = JSON.parse(body);
+          console.log(data)
+         //Sets the token info to match the var. We use the var to authenicate.
+         token.access_token = data.access_token;
+         token.refresh_token = data.refresh_token
+         token.scope = data.scope
+         token.creation = data.created_at
+         token.expire = data.expires_in
+         //Updates the DB with the info
+         authDB.update({}, { $set: { code: code, access_token: data.access_token, refresh_token: data.refresh_token, created_at: data.created_at, expire: data.expires_in } }, { multi: true }, function (err, numReplaced) {
+          console.log("Refreshed a token, ready to connect to chat!")
+          resolve("SUCCESS")}) 
+        } else {
+          console.log(body);
+          console.log(error)
+          resolve(error)
+        }
+      
+  })
+
+  })
   }
+
+async function updateID(client, secret) {
+  authDB.update({}, { $set: { clientID: client, secret: secret } }, { multi: true }, function (err, numReplaced) {
+    console.log("Updated the auth IDs.");
+  });
 }
 
-function checkForAuthWeb() {
-
+function getTokens() {
+  return token;
 }
 
-module.exports = { Auth, checkForAuthFile, recieveID, startAuthServer, updatePath }; //Send to the main file.
+
+
+module.exports = { Auth, makeAuth ,readAuth, recieveID, refreshToken, startAuthServer, updateID ,updatePath}; //Send to the main file.
