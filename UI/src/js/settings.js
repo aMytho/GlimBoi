f3e8fe90debe7f69cdb27cb0d0201a241dd810c4
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2021-05-02 14:21:05 -0500

    Settings UI rework

--- UI/src/js/settings.js
+++ UI/src/js/settings.js
@@ -22,6 +22,10 @@ let updatedSettings = {
         logging: false,
         filter: false,
         health: 0
+    },
+    music: {
+        chatAttribution: false,
+        writeToFile: false
     }
 }
 
@@ -70,6 +74,10 @@ function getSettings() {
                 logging: false,
                 filter: false,
                 health: 0
+            },
+            music: {
+                chatAttribution: false,
+                writeToFile: false
             }
         })
         try {
@@ -98,6 +106,10 @@ function getSettings() {
                 logging: false,
                 filter: false,
                 health: 0
+            },
+            music: {
+                chatAttribution: false,
+                writeToFile: false
             }
         }
     }
@@ -196,6 +208,14 @@ function showSettings() {
         default:
         break;
     }
+    // Music
+    if (settings.music.chatAttribution) {
+        document.getElementById("attributionMusicEnabled").toggleAttribute("checked");
+    }
+    if (settings.music.writeToFile) {
+        document.getElementById("fileMusicEnabled").toggleAttribute("checked");
+    }
+    document.getElementById("chatNavSelector").click();
 }
 
 
@@ -276,6 +296,10 @@ function saveSettings() {
             logging: document.getElementById("loggingEnabled").checked,
             filter: document.getElementById("filterEnabled").checked,
             health: getHealthInterval()
+        },
+        music: {
+            chatAttribution: document.getElementById("attributionMusicEnabled").checked,
+            writeToFile: document.getElementById("fileMusicEnabled").checked
         }
     }
     console.log(settings);
@@ -305,6 +329,10 @@ function resetSettings() {
             logging: false,
             filter: false,
             health: 0
+        },
+        music: {
+            chatAttribution: false,
+            writeToFile: false
         }
     }
     fs.writeFile(appData[1] + '/data/settings.json', JSON.stringify(settings), function () { });
@@ -313,7 +341,7 @@ function resetSettings() {
     successMessage("Settings Reset", "Your settings have been set to their original values.")
 }
 
-//applies the settings. THis is ran at launch after the file is read. Once finished the bot is fully ready
+//applies the settings. This is ran at launch after the file is read. Once finished the bot is fully ready
 function updateSettings() {
     CommandHandle.cooldownChange(settings.Commands.cooldown);
     ModHandle.updateFilter(settings.chat.filter);
