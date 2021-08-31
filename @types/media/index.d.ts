b58c6a3870912d727bf8671e715f3c21f69e1cd2
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2021-08-30 19:24:41 -0500

    Media type defs

--- @types/media/index.d.ts
+++ @types/media/index.d.ts
@@ -2,7 +2,26 @@ interface MediaType {
     name: mediaName;
     path: mediaPath;
     type: mediaType;
+    /**
+     * @deprecated
+     */
     position: mediaPosition;
+    duration: number
+}
+
+interface AudioType extends MediaType {
+    volume: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | number;
+}
+
+interface VideoType extends DisplayableMediaType {
+    speed: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | number;
+    volume: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | number;
+}
+
+interface DisplayableMediaType extends MediaType {
+    height: number;
+    width: number;
+    scale: number;
 }
 
 /**
