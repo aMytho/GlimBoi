b2f0a156df978cc608d380844eb03f4c7555ab73
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2022-04-05 15:16:35 -0500

    Use trigger system

--- lib/modules/chat/chatEvents.ts
+++ lib/modules/chat/chatEvents.ts
@@ -7,18 +7,20 @@ function handleNewFollower(follower: string) {
     if (CacheStore.get("followMessageEnabled", false)) {
         let message = CacheStore.get("followMessage", "Thanks for the follow $follower");
         message = message.replace("$follower", follower);
-        console.log(message, follower);
         ChatMessages.sendMessage(message);
-        LogHandle.logEvent({event: "New Follower", users: [follower], notification: `${follower} has just followed the stream!`});
     }
 
     if (CacheStore.get("streamlabsEnabled", false)) {
         let message = CacheStore.get("streamlabsMessage", "Thanks for the follow $follower");
         message = message.replace("$follower", `*${follower}*`);
-        console.log(message, follower);
         let StreamLabsModule:typeof import("../API/webhooks/streamlabs") = require(`${appData[0]}/modules/API/webhooks/streamlabs.js`);
         StreamLabsModule.triggerAlert(message, CacheStore.get("streamlabsToken", ""));
     }
+
+    let message = CacheStore.get("followMessage", "Thanks for the follow $follower");
+    message = message.replace("$follower", follower);
+    CommandHandle.TriggerHelper.checkContext("Follow", {user: follower, message: message});
+    LogHandle.logEvent({event: "New Follower", users: [follower], notification: `${follower} has just followed the stream!`});
 }
 
 export {handleNewFollower}
