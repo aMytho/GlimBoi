531ee0492b4f19aaed3d83a63407b62dd588512c
aMytho <JMyth07@outlook.com>
2022-06-14 21:15:05 +0000

    Remove old lib for angular conversion

--- lib/modules/API/websocket.ts
+++
@@ -1,5 +0,0 @@
-// file handles websocket connections
-
-const OBSWebSocket: typeof import("../API/websockets/OBS") = require(`${appData[0]}/modules/API/websockets/OBS.js`);
-
-export {OBSWebSocket}
